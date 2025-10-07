167bbb9ef63a28ed612798e1249eb6df9fac31b2
Vanlal Vena <vanlal.vena@drake.edu>
2025-10-07 15:59:34 -0500

    Add my image for homepage

--- myportfolio1/settings.py
+++ myportfolio1/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY
 SECRET_KEY = 'django-insecure-6%qw-nks^cwov)svid=1x^k(fc27)r*9x2e8-qh3fy&6#p3$1#'
 DEBUG = False
-ALLOWED_HOSTS = ['venav-e9epbyada8erehdq.centralus-01.azurewebsites.net']
+ALLOWED_HOSTS = ['venav-e9epbyada8erehdq.centralus-01.azurewebsites.net', '127.0.0.1', 'localhost',]
 #ALLOWED_HOSTS = ["*"]
 
 
